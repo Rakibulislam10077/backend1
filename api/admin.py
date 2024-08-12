ee0c4dc72188a1c16aad148bede04956612c685a
devs.rakib <devs.rakib@devsrakibs-MacBook-Pro.local>
2024-08-12 10:15:03 +0600

    add database

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from . models import Students
 # Register your models here.
+
+@admin.register(Students)
+# @admin.site.register(Students)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'roll', 'address', 'phone', 'email']
--- api/models.py
+++ api/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
 
-class student(models.Modal):
+class Students(models.Model):
     name = models.CharField(max_length=30)
     roll = models.IntegerField()
-    address = models.CharField(max_length=100)
-    phone = models.CharField(max_length=100)
+    address = models.CharField(max_length=200)
+    phone = models.CharField(max_length=11)
     email = models.CharField(max_length=100)
     image = models.ImageField(upload_to='media/')
     
